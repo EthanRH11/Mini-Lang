046dbedc92525ce42f5333eee012400ee4b2bb66
EthanHicks <ehicks12@kent.edu>
2024-12-17 20:44:19 -0500

    Worked on some parsing functions. A little complicated kind of falling behind on this project. I need to focus on finishing this parser then really work on cleaning up the code.

--- parser.hpp
+++ parser.hpp
@@ -10,7 +10,7 @@
 enum NODE_TYPE
 {
     NODE_ROOT,
-    NODE_ID,
+    NODE_VARIABLE,
     NODE_PRINT,
     NODE_RETURN,
     NODE_INT,
@@ -49,9 +49,61 @@ public:
             return current;
         }
     }
+    AST_NODE *parseKeywordEof()
+    {
+        /*Program is done executing*/
+        }
+    AST_NODE *parseKeywordPrint()
+    {
+        /*Must be with in a parenthesis to print*/
+    }
+
+    AST_NODE *parseKeywordInt()
+    {
+        /*Could set it up in 2 seperate ways
+          set it up to check if there is a value assigned to
+          the variable. or if the variable is just declared.*/
+        proceed(TOKEN_KEYWORD_INT);
+
+        std::string *variableName = &current->value;
+        proceed(TOKEN_IDENTIFIER);
+
+        AST_NODE *newNode = new AST_NODE();
+        newNode->TYPE = NODE_INT;
+        newNode->VALUE = variableName;
+
+        if (current->TYPE == TOKEN_EQUALS)
+        {
+            proceed(TOKEN_EQUALS);
+
+            if (current->TYPE != TOKEN_INTEGER_VAL)
+            {
+                std::cerr << "< Syntax Error > Expected an integer after '='\n";
+                exit(1);
+            }
+
+            AST_NODE *childNode = new AST_NODE();
+            childNode->TYPE = NODE_INT;
+            childNode->VALUE = &current->value;
+
+            newNode->CHILD = childNode;
+            proceed(TOKEN_INTEGER_VAL);
+        }
+        else
+        {
+            newNode->CHILD = nullptr;
+        }
+        return newNode;
+    }
 
     AST_NODE *parseID()
     {
+        std::string *buffer = &current->value;
+        proceed(TOKEN_IDENTIFIER);
+        proceed(TOKEN_EQUALS);
+
+        AST_NODE *newNode = new AST_NODE();
+        newNode->TYPE = NODE_VARIABLE;
     }
 
     AST_NODE *parse()
@@ -63,16 +115,21 @@ public:
         {
             switch (current->TYPE)
             {
-            case TOKEN_ID:
+            case TOKEN_IDENTIFIER:
             {
                 ROOT->SUB_STATEMENTS.push_back(parseID());
                 break;
             }
-            case TOKEN_KEYWORD:
+            case TOKEN_KEYWORD_INT: // COME BACK, WE HAVE MULTIPLE KEYWORDS
             {
-                ROOT->SUB_STATEMENTS.push_back(parseKeyword());
+                ROOT->SUB_STATEMENTS.push_back(parseKeywordInt());
                 break;
             }
+            case TOKEN_KEYWORD_PRINT:
+                ROOT->SUB_STATEMENTS.push_back(parseKeywordPrint());
+                break;
+            case TOKEN_EOF:
+                ROOT->SUB_STATEMENTS.push_back(parseKeywordEof());
             default:
             {
                 std::cerr << "< Syntax Error >" << std::endl;
