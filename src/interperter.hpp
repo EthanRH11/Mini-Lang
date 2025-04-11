94c85976847a4ad0f35740b7e5951900bcda4008
Ethan <ehicks12@kent.edu>
2025-04-11 14:03:29 -0400

    Attempted nested for loops, doesn;t work. Want to implement '\n' character to work as new line character

--- src/interperter.hpp
+++ src/interperter.hpp
@@ -43,11 +43,57 @@ private:
     Value evaluateExpression(AST_NODE *node); // Evaluates expressions such as (10 + x)
     void executeStatement(AST_NODE *node);
 
-    void printToOutput(const Value &val)
+    void printToOutput(const Value &value)
     {
-        if (outputFile.is_open())
+        // Print to console
+        if (value.isInteger())
         {
-            outputFile << val.toString() << std::endl;
+            std::cout << value.getInteger();
+            outputFile << value.getInteger();
+        }
+        else if (value.isDouble())
+        {
+            std::cout << value.getDouble();
+            outputFile << value.getDouble();
+        }
+        else if (value.isBool())
+        {
+            std::cout << (value.getBool() ? "true" : "false");
+            outputFile << (value.getBool() ? "true" : "false");
+        }
+        else if (value.isString())
+        {
+            // Check if the string contains newline characters
+            std::string str = value.getString();
+            bool containsNewline = str.find('\n') != std::string::npos;
+
+            std::cout << str;
+            outputFile << str;
+
+            // Only flush if the string contains a newline
+            if (containsNewline)
+            {
+                std::cout.flush();
+                outputFile.flush();
+            }
+        }
+        else if (value.isChar())
+        {
+            char ch = value.getChar();
+            std::cout << ch;
+            outputFile << ch;
+
+            // Only flush if the character is a newline
+            if (ch == '\n')
+            {
+                std::cout.flush();
+                outputFile.flush();
+            }
+        }
+        else
+        {
+            std::cout << "NULL";
+            outputFile << "NULL";
         }
     }
 
