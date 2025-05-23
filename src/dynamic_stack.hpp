9bd2a2b07f288cd0d92ae4d819b825c8d12788a4
Ethan <ehicks12@kent.edu>
2025-05-22 22:28:52 -0400

    Working on stack

--- src/dynamic_stack.hpp
+++ src/dynamic_stack.hpp
@@ -1,4 +1,45 @@
 #ifndef DYNAMIC_STACK_HPP
 #define DYNAMIC_STACK_HPP
 
+template <typename T>
+class Node
+{
+public:
+private:
+    T *data;
+    Node *
+};
+
+template <typename T>
+class Stack
+{
+public:
+    Stack();
+    ~Stack();
+    Stack &operator=(const Stack &);
+    Stack(const Stack &other);
+    Stack(Stack &&other);
+
+private:
+};
+
 #endif // DYNAMIC_STACK_HPP
+
+/*
+
+    //Head node points to node below it in the stack.
+
+    [NODE]
+       |            if adding a node to the stack, you would say
+       V           new->node->down = head;
+    [NODE]         head = new;
+       |
+       V
+    [NODE]          When removing a node to the stack you would say
+       |            Node* temp = head;
+       V            head = head->next;
+    [NODE]          delete temp;
+       |
+       V
+
+*/
